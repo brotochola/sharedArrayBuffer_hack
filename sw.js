71bfcc4814f1360970a7ffa7a5b2a0510193d240
saiegh@gmail.com <saiegh@gmail.com>
2025-11-15 14:48:12 -0300

    sep files

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).then((response) => {
+      const newHeaders = new Headers(response.headers);
+      newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+      newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+
+      return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: newHeaders,
+      });
+    })
+  );
+});
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
